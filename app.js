509a862e64a0d44f68732079dcac15c3e96896bc
Jacques van wyk <71254161+JacquesPMvanWyk@users.noreply.github.com>
2021-09-23 13:50:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var alertb = document.querySelector('.alertb')
+
+
+function getPassword(){
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
+    var passwordLength =40;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random()* chars.length);
+        password += chars.substring(randomNumber, randomNumber+1);
+    }
+    document.getElementById("password").value = password ;
+    alertb.innerHTML = "New Password Copied : <br> " + password;
+}
+
+function copyPassword(){
+    var copyPassText = document.getElementById("password");
+    copyPassText.select();
+    copyPassText.setSelectionRange(0,99999);
+    document.execCommand("copy");
+    alertb.classList.toggle('active')
+    setTimeout(function(){
+        alertb.classList.toggle('active')
+    },2000)
+
+}
